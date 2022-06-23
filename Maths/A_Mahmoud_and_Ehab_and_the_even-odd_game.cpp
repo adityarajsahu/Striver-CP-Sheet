9a43410d396da86cd383109eeae491d42c172e72
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2022-06-24 02:10:16 +0530

    new commit

---
+++ Maths/A_Mahmoud_and_Ehab_and_the_even-odd_game.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll N;
+    cin >> N;
+
+    if(N % 2 == 1) {
+        cout << "Ehab";
+    } else {
+        cout << "Mahmoud";
+    }
+    cout << endl;
+
+    return 0;
+}
