5b49617dfcf01ef8f70f8fc5e94adac325997ec2
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2022-07-21 18:52:21 +0530

    new commit

---
+++ Maths/A_Elephant.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<double,double> pd;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<pl> > vvpl;
+typedef vector<pl> vpl;
+typedef vector<pi> vpi;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define loop(i, k) for(int i=0 ; i<k ; i++)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ 
+
+int main()
+{
+    fast_cin();
+
+    ll N;
+    cin >> N;
+    
+    ll rem = N % 5;
+    if(rem == 0) {
+        cout << N / 5;
+    } else {
+        cout << (N / 5) + 1;
+    }
+    cout << endl;
+
+    return 0;
+}
