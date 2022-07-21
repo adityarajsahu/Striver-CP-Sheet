c0c7515a951f9c6f5974d5a5c7de64eba8d4144c
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2022-07-21 18:53:09 +0530

    new commit

---
+++ Maths/B_Emotes.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<double,double> pd;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<pl> > vvpl;
+typedef vector<pl> vpl;
+typedef vector<pi> vpi;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define loop(i, k) for(int i=0 ; i<k ; i++)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ 
+
+void solve()
+{
+    ll N, M, K;
+    cin >> N >> M >> K;
+    vl arr(N);
+    loop(i, N) {cin >> arr[i];}
+    sort(arr.begin(), arr.end(), greater<ll>());
+
+    ll steps = M / (K + 1);
+    ll ans = (arr[1] * steps) + (arr[0] * (M - steps));
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast_cin();
+    solve();
+    return 0;
+}
