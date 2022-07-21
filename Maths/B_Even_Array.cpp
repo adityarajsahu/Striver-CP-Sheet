0af30eabbb3d5f1cc5855d81ebe3164d6acf000e
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2022-07-21 18:52:49 +0530

    new commit

---
+++ Maths/B_Even_Array.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<double,double> pd;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<pl> > vvpl;
+typedef vector<pl> vpl;
+typedef vector<pi> vpi;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define loop(i, k) for(int i=0 ; i<k ; i++)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ 
+
+void solve()
+{
+    int N;
+    cin >> N;
+
+    vi arr(N);
+    loop(i, N) {
+        cin >> arr[i];
+    }
+
+    int even_idx = 0, odd_idx = 0;
+    loop(i, N) {
+        if(i % 2 == 0 && arr[i] % 2 == 1) {
+            even_idx++;
+        }
+
+        if(i % 2 == 1 && arr[i] % 2 == 0) {
+            odd_idx++;
+        }
+    }
+
+    if(even_idx == odd_idx) {
+        cout << even_idx << endl;
+    } else {
+        cout << -1 << endl;
+    }
+}
+
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for(int it=1;it<=t;it++) {
+        solve();
+    }
+    return 0;
+}
